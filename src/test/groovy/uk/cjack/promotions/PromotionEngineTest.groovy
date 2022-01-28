2ed0cf3341b540c48da19a621a86eed5b8a1acd6
Chris Jackson <chris@cjack.uk>
2022-01-28 23:17:34 +0000

    All Tests Passing

--- src/test/groovy/uk/cjack/promotions/PromotionEngineTest.groovy
+++ src/test/groovy/uk/cjack/promotions/PromotionEngineTest.groovy
@@ -43,12 +43,24 @@ class PromotionEngineTest extends Specification {
      */
     def "Scenario B"() {
         given: "An order contains 5 * A, 5 * B, and 1 * C"
+        final Order order = new Order(
+                new OrderLine("A"),
+                new OrderLine("A"),
+                new OrderLine("A"),
+                new OrderLine("A"),
+                new OrderLine("A"),
+                new OrderLine("B"),
+                new OrderLine("B"),
+                new OrderLine("B"),
+                new OrderLine("B"),
+                new OrderLine("B"),
+                new OrderLine("C"))
 
         when: "I call the promotion engine with the given order"
         promotionEngine = new PromotionEngine(order)
 
         then: "Then total order value should be 370"
-        promotionEngine.calculateValue(order.getOrderMap()) == 370
+        promotionEngine.getOrderTotal() == 370
     }
 
     /**
@@ -61,11 +73,22 @@ class PromotionEngineTest extends Specification {
      */
     def "Scenario C"() {
         given: "An order contains 3 * A, 5 * B, 1 * C, and 1 * D"
+        final Order order = new Order(
+                new OrderLine("A"),
+                new OrderLine("A"),
+                new OrderLine("A"),
+                new OrderLine("B"),
+                new OrderLine("B"),
+                new OrderLine("B"),
+                new OrderLine("B"),
+                new OrderLine("B"),
+                new OrderLine("C"),
+                new OrderLine("D"))
 
         when: "I call the promotion engine with the given order"
         promotionEngine = new PromotionEngine(order)
 
         then: "Then total order value should be 100"
-        promotionEngine.calculateValue(order.getOrderMap()) == 280
+        promotionEngine.getOrderTotal() == 280
     }
 }
